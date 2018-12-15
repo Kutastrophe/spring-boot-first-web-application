8798320f5e84066d107f527a51699b253886807b
Kutastrophe <marvin.hohbach@web.de>
2018-12-15 21:09:19 +0100

    .jsp funtzt
    Internal Recourceviewcontroller hat sich mit spring 5 verändert d.h
    
    public class mvcbla IMPLEMENTS WebMvcConfigurer (nicht wie vorher extends)

--- src/main/java/com/in28mins/springboot/web/springbootfirstwebapplication/controller/LoginController.java
+++ src/main/java/com/in28mins/springboot/web/springbootfirstwebapplication/controller/LoginController.java
@@ -1,8 +1,8 @@
-package com.in28mins.springboot.web.springbootfirstwebapplication;
+package com.in28mins.springboot.web.springbootfirstwebapplication.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+//import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
